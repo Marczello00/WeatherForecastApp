d5e80766fcf02692a7089188193777b39b93af66
Marczello00 <56977890+Marczello00@users.noreply.github.com>
2024-01-08 00:14:20 +0000

    small visual change

--- views/MainPage.py
+++ views/MainPage.py
@@ -21,7 +21,7 @@ class MainPage:
         s.configure('Frame1.TFrame')
 
         self.root.columnconfigure(0, weight=1)
-        self.root.rowconfigure(0, weight=1)
+        self.root.rowconfigure(0, weight=0)
         self.root.rowconfigure(1, weight=1)
         self.root.rowconfigure(2, weight=1)
         self.root.rowconfigure(3, weight=1)
